c8a39f74e680b608edb3668866686327bbff19a3
ghiloufi <belgacemghiloufi2021@gmail.com>
2022-10-24 20:55:25 +0200

    refactor: refactor the ConsoleStatementPrinter class

--- src/main/java/fr/tdd/kata/bankaccount/adapters/ConsoleStatementPrinter.java
+++ src/main/java/fr/tdd/kata/bankaccount/adapters/ConsoleStatementPrinter.java
@@ -4,7 +4,6 @@ import java.math.BigDecimal;
 import java.text.DecimalFormat;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
-import java.util.function.Function;
 
 import fr.tdd.kata.bankaccount.domain.model.OperationType;
 import fr.tdd.kata.bankaccount.domain.model.Transaction;
@@ -23,24 +22,26 @@ public class ConsoleStatementPrinter implements StatementPrinter {
 	@Override
 	public void print(List<Transaction> transactions) {
 		console.printLine(STATEMENT_HEADER);
-		
-		DecimalFormat decimalFormat = new DecimalFormat("#.00");
-		
 		AtomicReference<BigDecimal> runningBalance = new AtomicReference<BigDecimal>(new BigDecimal(0.00));
-		
-		Function<Transaction, String> transactionToStatementLineConverter = transaction -> {
-			var amount = (transaction.getOperationType() == OperationType.DEPOSIT) ? transaction.getAmount() : transaction.getAmount().negate();
-			String statementLine = transaction.getOperationType()
-															   + " | " + 
-															   transaction.getDate() 
-															   + " | " + 
-															   decimalFormat.format(amount)
-															   + " | " + 
-															   decimalFormat.format(runningBalance.accumulateAndGet(amount, (u,v) -> u.add(v))); 
-			return statementLine;};
-			
-			transactions.stream().map(transactionToStatementLineConverter).forEach(console::printLine);
-		
+	    transactions.stream()
+							.map(transaction ->transactionToStatementLineConverter(transaction, runningBalance))
+							.forEach(console::printLine);
+	}
+	
+	private String transactionToStatementLineConverter(Transaction transaction, AtomicReference<BigDecimal> runningBalance) {
+		var amount = (transaction.getOperationType() == OperationType.DEPOSIT) ? transaction.getAmount() : transaction.getAmount().negate();
+		return transaction.getOperationType() 
+				   + " | " +
+				   transaction.getDate()
+				   + " | " +
+				   formatDecimal(amount)
+				   + " | " +
+				   formatDecimal(runningBalance.accumulateAndGet(amount, (u,v) -> u.add(v)));
+	}
+	
+	private String formatDecimal(BigDecimal amount) {
+		DecimalFormat decimalFormat = new DecimalFormat("#.00");
+		return decimalFormat.format(amount);
 	}
 
 }
